c4cc2d47c462cd78b90d868c99b78c30845ad9b8
paulacd <pcd250@nyu.edu>
2017-04-09 17:04:49 -0400

    removed vibrantjs, added server

---
+++ config.js
@@ -0,0 +1,29 @@
+var cfg = {};
+
+// HTTP Port to run our web application
+cfg.port = process.env.PORT || 3000;
+
+// A random string that will help generate secure one-time passwords and
+// HTTP sessions
+cfg.secret = process.env.APP_SECRET || 'keyboard cat';
+
+// Your Twilio account SID and auth token, both found at:
+// https://www.twilio.com/user/account
+// 
+// A good practice is to store these string values as system environment
+// variables, and load them from there as we are doing below. Alternately,
+// you could hard code these values here as strings.
+cfg.accountSid = 'AC2731947b3b1632cf9a7b5ca51c17cd6f';
+cfg.authToken = '5451a6544eda8e8d9758d3e35d5e9332';
+
+// A Twilio number you control - choose one from:
+// https://www.twilio.com/user/account/phone-numbers/incoming
+// Specify in E.164 format, e.g. "+16519998877"
+cfg.twilioNumber = process.env.TWILIO_NUMBER;
+
+// MongoDB connection string - MONGO_URL is for local dev,
+// MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
+cfg.mongoUrl = process.env.MONGOLAB_URI || process.env.MONGO_URL
+
+// Export configuration object
+module.exports = cfg;
